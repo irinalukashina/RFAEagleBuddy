28fd13143eaaa3dafcc7b15769faca48237d832e
irinalukashina <irina.lukashina@gmail.com>
2017-09-26 18:58:59 -0700

    Fixed route

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/eagle/test/qq', api);
+app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
